fa6c9f1cc0d94140c99a502c424d5b9a8f2c21a8
Alex Reznichenko <alex.r@itmaster-soft.com>
2022-01-13 08:36:39 +0200

    JSON db and simple chart

--- src/app/views/dashboard/dashboard-default-content.tsx
+++ src/app/views/dashboard/dashboard-default-content.tsx
@@ -1,9 +1,146 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Theme } from '@mui/material/styles';
+import makeStyles from '@mui/styles/makeStyles';
+import { SaleType } from 'models/sale-type';
+import { getSalesAxios } from 'services/saleService';
+import { useTheme } from '@mui/system';
+import Chart from 'react-apexcharts';
+import {
+  Box,
+  Card,
+  CardContent,
+  Container,
+  Grid,
+  Typography,
+} from '@mui/material';
+import Page from 'app/components/page';
+
+const useStyles = makeStyles(() => ({
+  root: {
+    minHeight: '100%',
+  },
+}));
+
+const getChartStyling = (theme: Theme) => ({
+  chart: {
+    background: theme.palette.background.paper,
+    toolbar: {
+      show: false,
+    },
+  },
+  colors: ['#13affe', '#fbab49'],
+  dataLabels: {
+    enabled: false,
+  },
+  grid: {
+    borderColor: theme.palette.divider,
+    yaxis: {
+      lines: {
+        show: false,
+      },
+    },
+  },
+  legend: {
+    show: true,
+    labels: {
+      colors: theme.palette.text.secondary,
+    },
+  },
+  plotOptions: {
+    bar: {
+      columnWidth: '40%',
+    },
+  },
+  stroke: {
+    show: true,
+    width: 2,
+    colors: ['transparent'],
+  },
+  theme: {
+    mode: theme.palette.mode,
+  },
+  tooltip: {
+    theme: theme.palette.mode,
+  },
+  xaxis: {
+    axisBorder: {
+      show: true,
+      color: theme.palette.divider,
+    },
+    axisTicks: {
+      show: true,
+      color: theme.palette.divider,
+    },
+    categories: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+    labels: {
+      style: {
+        colors: theme.palette.text.secondary,
+      },
+    },
+  },
+  yaxis: {
+    axisBorder: {
+      show: true,
+      color: theme.palette.divider,
+    },
+    axisTicks: {
+      show: true,
+      color: theme.palette.divider,
+    },
+    labels: {
+      style: {
+        colors: theme.palette.text.secondary,
+      },
+    },
+  },
+});
+
 const DashboardDefaultContent = () => {
+  const classes = useStyles();
+  const theme = useTheme<Theme>();
+  const [sales, setSales] = useState<SaleType[]>([]);
+
+  useEffect(() => {
+    let isMounted = true;
+    const loadSales = async () => {
+      const { data: sales } = await getSalesAxios();
+      isMounted && setSales(sales);
+    };
+    loadSales();
+    return () => {
+      isMounted = false;
+    };
+  }, []);
+
+  console.log(sales);
+
   return (
-    <div>
-      <h1>Dashboard Default Content</h1>
-    </div>
+    <Page className={classes.root} title="Dashboard">
+      <Container maxWidth={'sm'}>
+        <Typography variant="h4" color="textPrimary">
+          Dashboard
+        </Typography>
+        <Box my={5}>
+          <Grid container spacing={3}>
+            <Grid item xs={12}>
+              <Card>
+                <CardContent>
+                  <Typography variant="h5" color="textPrimary">
+                    Sales
+                  </Typography>
+                  <Chart
+                    options={getChartStyling(theme)}
+                    series={sales}
+                    type="bar"
+                    height={'100%'}
+                  />
+                </CardContent>
+              </Card>
+            </Grid>
+          </Grid>
+        </Box>
+      </Container>
+    </Page>
   );
 };
 
---
+++ src/services/saleService.ts
@@ -0,0 +1,12 @@
+import api, { EndPoints } from 'api/axios';
+import { SaleType } from 'models/sale-type';
+
+export async function getSalesAxios() {
+  return await api.get<SaleType[]>(EndPoints.sales);
+}
+/* Other commonly-used api methods:
+  api.post
+  api.put
+  api.delete
+  api.patch
+*/
